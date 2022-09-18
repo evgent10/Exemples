d797c0a23d0bd21821a252db1b4d6f626d5a2221
evgent10 <evgent10@ya.ru>
2022-09-18 19:18:46 +0300

    Examples010_MetodArray

---
+++ Examples010_MetodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 21, 13, 4, 51, 16, 71, 18};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
